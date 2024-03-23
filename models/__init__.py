730408b46130473a85d23a22cceba4e80ce78e86
Djimra NGARLEITA <dngarleita@gmail.com>
2024-03-23 14:45:30 +0300

    fix: add unit tests for BaseModel

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Package initializer"""
 from models.engine.file_storage import FileStorage
 
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,8 @@
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
+from io import StringIO
+from unittest.mock import patch
 
 
 class TestBaseModel(unittest.TestCase):
@@ -15,3 +17,32 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(new_model.id, str)
         self.assertIsInstance(new_model.created_at, datetime)
         self.assertIsInstance(new_model.updated_at, datetime)
+
+    def test_save_method_will_update_updated_at(self):
+        """Check updated_at property is updated when save() is called"""
+        b_model = BaseModel()
+        initial_datetime = b_model.updated_at
+        b_model.save()
+        self.assertNotEqual(initial_datetime, b_model.updated_at)
+
+    def test_str_method_return_a_string_with_expected_format(self):
+        """Test that the __str__() method return the correct string"""
+        b_model = BaseModel()
+        str_rep = str(b_model)
+        self.assertIsInstance(str_rep, str)
+        self.assertIn('[BaseModel]', str_rep)
+        self.assertIn("({})".format(b_model.id), str_rep)
+        self.assertIn('created_at', str_rep)
+        self.assertIn('updated_at', str_rep)
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            print(b_model)
+            self.assertEqual(fake_out.getvalue().rstrip('\n'), str_rep)
+
+    def test_to_dict(self):
+        """Test that to dict method returns the correct dict with all keys"""
+        model = BaseModel()
+        dict_rep = model.to_dict()
+        self.assertEqual(dict_rep['id'], model.id)
+        self.assertEqual(dict_rep['__class__'], type(model).__name__)
+        self.assertEqual(dict_rep['created_at'], model.created_at.isoformat())
+        self.assertEqual(dict_rep['updated_at'], model.updated_at.isoformat())
