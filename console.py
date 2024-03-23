7cd0c83973c00cbc0fa7fb3c81d73b741ee2ccbc
Djimra NGARLEITA <dngarleita@gmail.com>
2024-03-23 20:27:01 +0300

    fix: implemente emptyline()

--- console.py
+++ console.py
@@ -7,6 +7,18 @@ class HBNBCommand(cmd.Cmd):
     """Command line interpreter for the AirBnB clone project"""
     prompt = '(hbnb) '
 
+    def emptyline(self):
+        """Read empty line and pass"""
+        pass
+
+    def precmd(self, line):
+        """Read and sanitize the user input before cmd"""
+        if line:
+            line = line.strip()
+        if isinstance(line, str):
+            return line
+        self.emptyline()
+
     def do_EOF(self, line):
         """Exit this interactive shell"""
         print('')
